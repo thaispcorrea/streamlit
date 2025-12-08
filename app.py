b86f386063eeff6a7fe0754a2dc9ddf06a0d9849
thaispcorrea <thaispcorrea@gmail.com>
2025-12-08 17:10:36 +0100

    Ajustes finais: renomeado arquivo para vehicles_us.csv e pasta para .streamlit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('notebooks/vehicles.csv') # lendo os dados
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
         
 if hist_button: # se o botão for clicado
